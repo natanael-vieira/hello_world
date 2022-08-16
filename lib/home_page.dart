f5c7df5d8f0f405dd65ef38a524f763a4fc55b69
Natanael Vieira <natanaelsales@live.com>
2022-08-16 09:30:25 -0300

    Update hello_word
    Mudança do contador de StateLess para StateFull

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,22 +1,59 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:hello_world/home_controller.dart';
+
+/*class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var controller = HomeController.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('HelloWorld!'),
+      ),
+      body: Center(
+        child: Text('Clicou ${controller.value}'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          controller.increment();
+        },
+      ),
+    );
+  }
+}
+*/
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomePage'),
+        title: Text('HelloWorld!'),
       ),
       body: Center(
-        child: Text('HelloWorld'),
+        child: Text('Clicou $counter'),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
-        print('Clicou');
-      },
+          setState(() {
+            counter++;
+          });
+          
+        },
       ),
     );
   }
--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/home_controller.dart';
 
 import 'home_page.dart';
 
@@ -7,8 +8,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return MaterialApp(
+      home: HomeController(
+        child: HomePage(),
+      ),
     );
   }
 }
