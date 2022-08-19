259c53da0a308756e17a4fcf1169642bc18c3c3a
Natanael Vieira <natanaelsales@live.com>
2022-08-19 06:00:57 -0300

    Update hello_world
    Utilização de imagens internas e externas (link internet) juntamente com o Stack para empilhar (sobrepor) itens uns sobre os outros.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,52 +8,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
-  final controller = TextEditingController();
-  final list = <String>[];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HelloWorld!'),
-      ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: controller,
-                  ),
+        appBar: AppBar(
+          title: const Text('HelloWorld!'),
+        ),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/img/nasa.jpg',
+                  fit: BoxFit.cover,
                 ),
-                IconButton(
-                    onPressed: () {
-                      final text = controller.text;
-                      list.add(text);
-                      setState(() {
-                     
-                      });
-                      controller.clear();
-                    },
-                    icon: const Icon(Icons.add)),
-              ],
-            ),
+                //Image.network('http://static.vecteezy.com/packs/media/components/global/search-explore-nav/img/vectors/term-bg-1-666de2d941529c25aa511dc18d727160.jpg', fit: BoxFit.cover,),
+              ),
+              Container(
+                color: Colors.black,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.white,
+                height: 85,
+                width: 85,
+              ),
+              Container(
+                color: Colors.black,
+                height: 70,
+                width: 70,
+              ),
+            ],
           ),
-          Expanded(
-            child: ListView.builder(
-                itemCount: list.length,
-                itemBuilder: (context, index) {
-                  final item = list[index];
-                  return ListTile(
-                    title: Text(item),
-                  );
-                }),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
